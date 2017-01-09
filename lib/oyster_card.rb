c1f8425d2c62227357ca8f6cf8cc863ffacbfb5c
Ross Benzie <rossbenzie@Ross-MacBook-Pro.local>
2017-01-09 17:15:11 +0000

    Added MAX_BALANCE

--- lib/oyster_card.rb
+++ lib/oyster_card.rb
@@ -1,12 +1,13 @@
 class OysterCard
   attr_reader :balance
+  MAX_BALANCE = 90
 
   def initialize
     @balance = 0
   end
 
   def top_up(amount)
-    raise "Attempted to top up beyond max value" if @balance + amount > 90
+    raise "Attempted to top up beyond max value" if @balance + amount > MAX_BALANCE
     @balance += amount
   end
 
