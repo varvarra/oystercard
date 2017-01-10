66f4869ab33558debea421fae520194a0f677041
Jack <92jackhenderson@gmail.com>
2017-01-10 11:30:19 +0000

    myorigin update

--- spec/oyster_card_spec.rb
+++ spec/oyster_card_spec.rb
@@ -11,11 +11,8 @@ describe OysterCard do
       subject.top_up(20)
       expect(subject.top_up(20)).to eq 40
     end
-    it "Stops top ups beyond max balance" do
-      max_balance = OysterCard::MAX_BALANCE
-      message = "Attempted to top up beyond max value of £#{max_balance}.00"
-      expect {subject.top_up(max_balance+0.01)}.to raise_error(message)
-    end
   end
 
+    subject.top_up(>90)
+    expect raise_error 
 end
